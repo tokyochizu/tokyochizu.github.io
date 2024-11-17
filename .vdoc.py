f08431f47020b654728d2f34fc3d744ca4e6ed11
tokyochizu <188234558+tokyochizu@users.noreply.github.com>
2024-11-17 10:27:38 -0500

    Add Zoning/Layout

--- .vdoc.py
+++ .vdoc.py
@@ -7,14 +7,20 @@
 #
 #
 #
+#
+#
+#
+#
+#
+#
+#
 import geopandas as gpd
+import folium
 
-blocks = gpd.read_file("../ASS2/GIS/bounds/r2ka13.shp")
-blocks = blocks[blocks.CITY_NAME.notna()]
-blocks = blocks[(blocks.CITY_NAME.str.contains("区"))]
-blocks["cityblockmerge"] = blocks.CITY_NAME.str.strip() + blocks.S_NAME.str.strip()
-blocks = blocks.drop(blocks.columns[[0, 1, 2, 3, 4, 7, 8, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28]], axis=1)
 
+wardlist = ["千代田区", "中央区", "港区", "新宿区", "文京区", "台東区", "墨田区", "江東区", 
+        "品川区", "目黒区", "大田区", "世田谷区", "渋谷区", "中野区", "杉並区", "豊島区", "北区", 
+        "荒川区", "板橋区", "練馬区", "足立区", "葛飾区", "江戸川区"]
 ward2roman = {
     "千代田区": "Chiyoda",
     "中央区": "Chuo",
@@ -40,26 +46,51 @@ ward2roman = {
     "葛飾区": "Katsushika",
     "江戸川区": "Edogawa"
 }
-import pandas as pd
-disaster = pd.read_excel("../ASS2/disasteredit.xlsx")
-disaster.columns = ["Ward", "Block", "Geo Class", "Building Collapse Risk (Building / ha)", "Building Collapse Risk Ranking", "Building Risk Grade", 
-                    "Fire Risk (Building / ha)", "Fire Risk Ranking", "Fire Risk Grade", "Disaster Relief Difficulty Rating", 
-                    "Combined Risk (Building / ha)", "Combined Risk Ranking", "Combined Risk Grade"]
-disaster = disaster[(disaster.Ward.str.contains("区"))]
-disaster["blockmergename"] = disaster.Ward + disaster.Block
-disaster = disaster.drop(disaster.columns[[4, 7, 11]], axis=1)
 
-num = {"１": "一", "２": "二", "３": "三", "４": "四", "５": "五", "６": "六", "７": "七", "８": "八", "９": "九"}
-chars = {"ケ": "ヶ"}
-for k, v in num.items():
-    disaster.blockmergename = disaster.blockmergename.str.replace(k,v)
-for k, v in chars.items():
-    blocks.cityblockmerge = blocks.cityblockmerge.str.replace(k,v)
-    disaster.blockmergename = disaster.blockmergename.str.replace(k,v)
-
-merged_df = blocks.merge(disaster, left_on="cityblockmerge", right_on="blockmergename", how="outer",indicator=True)
+wards = gpd.read_file("../GIS/tokyoshp/N03-20240101_13.shp")
+wards = wards[wards.N03_004.isin(wardlist)]
+wards = wards.drop(wards.columns[[0, 1, 2, 4, 5]], axis=1)
+wards.columns = ["Ward", "geometry"]
+wards["Ward Rom"] = wards["Ward"].map(ward2roman)
 
 
+blocks = gpd.read_file("../ASS2/GIS/bounds/r2ka13.shp")
+blocks = blocks[blocks.CITY_NAME.notna()]
+blocks = blocks[(blocks.CITY_NAME.str.contains("区"))]
+blocks = blocks.drop(blocks.columns[[0, 1, 2, 3, 4, 7, 8, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28]], axis=1)
+blocks.columns = ["Ward", "Block", "Area", "geometry"]
+blocks["Ward Rom"] = blocks["Ward"].map(ward2roman)
+blocks.insert(1, "Ward Rom", blocks.pop("Ward Rom"))
+
+m = folium.Map([35.66, 139.74], zoom_start=11)
+
+folium.GeoJson(
+    blocks,
+    name="Blocks",
+    style_function=lambda x: {
+        "fillColor": "#ff8700",
+        "color": "#000000",
+        "fillOpacity": 0.3,
+        "weight": 1
+    }, 
+    show=False,
+    tooltip=folium.GeoJsonTooltip(fields=["Ward", "Ward Rom", "Block", "Area"])
+).add_to(m)
+
+folium.GeoJson(
+    wards,
+    name="Wards",
+    style_function=lambda x: {
+        "fillColor": "#0078ff",
+        "color": "#000000",
+        "fillOpacity": 0.3,
+        "weight": 2
+    },
+    tooltip=folium.GeoJsonTooltip(fields=["Ward", "Ward Rom"])
+).add_to(m)
+
+folium.LayerControl().add_to(m)
+m
 #
 #
 #
